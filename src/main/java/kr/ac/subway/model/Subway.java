00d570457ee334c4f53b9abeab0e4fd67d46df21
LeeSeoHyeon <LeeSeoHyeon@lsh>
2017-02-27 12:31:58 +0900

    데이터 베이스 연동과 아두이노 센서에서 온 값들 데이터 베이스로 전달

--- src/main/java/kr/ac/subway/model/Subway.java
+++ src/main/java/kr/ac/subway/model/Subway.java
@@ -1,5 +1,54 @@
 package kr.ac.subway.model;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class Subway {
 
+	private String date;
+	@NotEmpty(message="Temperature is not empty")
+	private String temperature;
+	@NotEmpty(message="Humidity is not empty")
+	private String humidity;
+	
+	
+	public Subway() {
+	
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Subway [date=" + date + ", temperature=" + temperature + ", humidity=" + humidity + "]";
+	}
+
+
+	public Subway(String date, String temperature, String humidity) {
+		this.date = date;
+		this.temperature = temperature;
+		this.humidity = humidity;
+	}
+	public String getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getHumidity() {
+		return humidity;
+	}
+	public void setHumidity(String humidity) {
+		this.humidity = humidity;
+	}
+	
+	
+	
 }
